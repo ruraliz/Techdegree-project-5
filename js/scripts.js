fe4163a5d1d66bbae0bbdeaa3205eef52d9add7f
Liz Rurangwa <107972391+ruraliz@users.noreply.github.com>
2023-09-28 20:14:07 -0700

    Added modals that display employee info and close button to close the modal

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,6 @@
+
+const container= document.querySelector('.gallery')
+let employees= [];
 async function getEmployees(){
     try{
     const response = await fetch('https://randomuser.me/api/?nat=us&results=12')
@@ -10,7 +13,6 @@ async function getEmployees(){
         console.log(error)
     }
 }
-
 function displayEmployees(employees){
     employees.map(employee => {
         const employeeHTML= `
@@ -18,14 +20,65 @@ function displayEmployees(employees){
                 <div class="card-img-container">
                     <img class="card-img" src=${employee.picture.large} alt="profile picture">
                 </div>
-                <div class="card-info-container">
+                <div class="card-info-container" data-name=${employee.name.first}>
                     <h3 id="name" class="card-name cap">${employee.name.first} ${employee.name.last}</h3>
                     <p class="card-text">${employee.email}</p>
                     <p class="card-text cap">${employee.location.city}, ${employee.location.state}</p>
                 </div>
             </div>
         `;
-        document.querySelector('.gallery').insertAdjacentHTML('beforeend', employeeHTML)
+       container.insertAdjacentHTML('beforeend', employeeHTML)
+       const employeeCard= document.querySelector('.card')
+        employeeCard.addEventListener('click', (e)=> {
+            const card= e.target.closest('.card-info-container');
+            if(!card) return;
+            const employeeName= card.dataset.name;
+            const employee= employees.find(
+                (employee) => employee.name.first === employeeName
+            );
+            employeeModal(employee);
+        });
     });
 }
 getEmployees();
+
+function employeeModal(employee){
+    const modalHTML= `
+    <div class="modal-container">
+        <div class="modal">
+            <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+            <div class="modal-info-container">
+                <img class="modal-img" src=${employee.picture.large} alt=${employee.name.first} picture>
+                <h3 id="name" class="modal-name cap">${employee.name.first} ${employee.name.last}</h3>
+                <p class="modal-text">${employee.email}</p>
+                <p class="modal-text cap">${employee.location.city}</p>
+                <hr>
+                <p class="modal-text">${employee.cell}</p>
+                <p class="modal-text">${employee.location.street.number} ${employee.location.street.name}., ${employee.location.city}, ${employee.location.state} ${employee.location.postcode}</p>
+                <p class="modal-text">Birthday: ${employee.dob.date}</p>
+            </div>
+        </div>
+            <div class="modal-btn-container">
+            <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
+            <button type="button" id="modal-next" class="modal-next btn">Next</button>
+            </div>
+    </div>
+    `; 
+    const modals= document.querySelectorAll('.modal-container')
+    container.insertAdjacentHTML('afterend', modalHTML)
+    modals.forEach(modal => {
+        modal.style.display= "none";
+    })
+    const closeButton= document.querySelectorAll('.modal-close-btn')
+    closeButton.forEach(button => 
+        button.addEventListener('click', () => {
+            console.log('clicked')
+        const modalContainer= document.querySelector('.modal-container')
+        if(modalContainer){
+            modalContainer.remove();
+        }
+        })
+    )
+
+}
+
